3bb7a44732478c54eacdfc7175fc2514ba484737
Justin Slud <justinslud@yahoo.com>
2020-10-26 12:52:50 -0400

    remove logger formatter

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -2,10 +2,10 @@ import logging
 
 logger = logging.getLogger(name='logger')
 formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(funcName)s : %(message)s')
-logger.setFormatter(formatter)
+# logger.setFormatter(formatter)
 logger.setLevel(logging.DEBUG)
 
 filehandler = logging.FileHandler('log.txt')
 filehandler.setLevel(logging.DEBUG)
-filehandler.setFormatter(formatter)
+# filehandler.setFormatter(formatter)
 logger.addHandler(filehandler)
