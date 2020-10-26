dbdbd6346f17bb2c1a6daacc499c2580feb718cf
Justin Slud <justinslud@yahoo.com>
2020-10-26 12:23:17 -0400

    rename scrapers folder to scrape and add extract/pdf.py

---
+++ src/extract/pdf.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import re
+from tika import parser
+from cleantext import clean
+
+def extract_pdf(url):
+  '''
+  Name: extract_pdf
+  Description: Takes user input of pdf url/link of academic paper and 
+  extracts relevant information. 
+  Input: 
+  @url: the string of link to pdf of requested academic paper
+  Output: abstract, conclusion, references, text
+  '''
+
+  # Parse PDF from url
+  pdfFile = parser.from_file(url)
+  text = pdfFile["content"].strip()
+
+  # do minmal cleaning on text 
+  text = clean(text,no_line_breaks=True)
+  # print(text)
+  abstract = conclusion = references = 'NA'
+
+#   title_author = text.split(re.search('(?i)Abstract', text)[0],1)[0].strip().replace('\n\n', '\n')
+
+  #Get ABSTRACT
+  try:
+    # Find keyword in text and split to remove leading whitespace after Keyword.
+    search = '(?i)Abstract'
+    keyword = re.search(search, text)[0]
+    post_abstract = text.split(keyword,1)[1].strip()
+
+    # Extract subtring (Abstract) until two newlines detected
+    abstract = post_abstract[:post_abstract.index('\n\n')].strip()
+
+  except:
+    pass
+
+  #Get CONCLUSION
+  try:
+    #search parameters for conclusion
+    search = '(?i)(Conclusion(s)|Summary|Discussion)'
+
+    keyword = re.search(search,text)[0]
+
+    #split text to after keyword found
+    post_conclusion = text.split(keyword,1)[1].strip()
+
+    #index keyword 'refrences in split text containing conclusion(s)
+    end = re.search('(?i)(References|Bibliiography|Works Cited|Sources|Citations)',post_conclusion).start()
+
+    #Extract text from 'conclusion' keyword to 'references' keyword.
+    conclusion = post_conclusion[:end-1].strip()
+    #conclusion = post_conclusion[:post_conclusion.index('\n\n')].strip()
+  
+  except:
+    conclusion = text[-2000:-500]
+
+  #Get CITATIONS and FULL BODY TEXT WITHOUT CITATIONS
+  try:
+    # list of indicators/key words for the "citations" category 
+    search = '(?i)(References|Bibliiography|Works Cited|Sources|Citations)'
+
+    #Find keyword used from list
+    keyword = re.search(search,text)[0]
+
+    #Split on keyword and extract everything after match
+    citations = text.split(keyword,1)[1].strip()
+
+    #return the full body text of the article without the citations.
+    fulltext = text.split(keyword,1)[0].strip() 
+
+
+
+    #Create Exception to remove Appendix or Acknowledgemnt section
+    if (re.search('(?i)(Appendix|Acknowledgement)',citations)):
+
+      keyword2 = re.search('(?i)(Appendix|Acknowledgement)',citations)[0]
+
+      #citations is between 'citations' keyword and 'appendix; keyword.
+      references = references.split(keyword2,1)[0].strip()
+  
+  except:
+    fulltext = text
+    references = text[-500:]
+
+
+  for data in [abstract, conclusion, references, fulltext]:
+    if type(data) == str:
+      data = data.replace('\n', '')
+
+
+  references = [references] if references else 'NA'
+
+  return pd.Series(data=[abstract, conclusion, references, text], index=['Abstract', 'Conclusion', 'References', 'Text'])
---
+++ src/scrape/master.py
@@ -0,0 +1,85 @@
+from .. import logger
+from scrapers.extract import extract_text
+import arxiv 
+
+db_fctns = {
+    #'core': scrape_core,
+            # 'elsevier': scrape_elsevier,
+            'arxiv': scrape_arxiv
+            # 'dpla': scrape_dpla,
+            # 'ncbi': scrape_ncbi
+            # 'ieee': scrape_ieee
+            # 'springer': scrape_springer,
+            # 'wiley': scrape_wiley,
+            # 'plos': scrape_plos,
+            # 'crossref': scrape_crossref,
+            # 'google': scrape_google,
+            # 'scholar': scrape_scholar,
+            # 'wikipedia': scrape_wikipedia
+            }
+
+
+
+def master_search(query, pages=0, year=None, databases=None):
+  '''
+  Input:
+    query: search term
+    pages: how many pages to get from each database
+    year: start year
+    databases: list of databases
+  Output:
+    pandas DataFrame
+  '''
+
+  dfs = []
+
+  # if no database specified
+  if databases is None:
+
+    # use all databases 
+    databases = db_fctns.keys()
+
+  for database in databases:
+
+    # check database exists
+    if database.lower() in db_fctns.keys():
+
+      # get corresponding scraper function
+      scraper_fctn = db_fctns[database]
+
+      try:
+
+        # scrape specified database with parameters
+        rows = scraper_fctn(query, pages, year)
+
+      except Exception as e:
+        print(e, database)
+        continue
+        # logging.error(f'{e}')
+
+      if len(rows) > 0:
+        dfs.append(rows)
+        
+        #logger.debug(f'Scraped {len(dfs[-1])} articles from {database} for "{query}"')
+
+      #else:
+        #logger.warning(f'Could not scrape articles from {database} for "{query}"')
+
+  updated_dfs = []
+
+  for df in dfs:
+    
+    try:
+    
+      # update Abstract, Conclusion, References, and Text columns by extracting
+      updated_dfs.append(extract_text(df))
+      
+      logger.debug(f'Extracted text from {len(updated_dfs[-1])} articles from {database} for "{query}"')
+    
+    except:
+      logger.warning(f'Could not extract text from {database} for "{query}"')
+
+  # combine DataFrames
+  if len(updated_dfs) > 0:
+    
+    return pd.concat(updated_dfs)
