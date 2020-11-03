4f0f3e1adce33a469305b9efde24223494f75e52
justinslud <43285909+justinslud@users.noreply.github.com>
2020-11-03 18:41:41 -0500

    put into 1 try/except block

--- scrapers/extract/scihub.py
+++ scrapers/extract/scihub.py
@@ -16,32 +16,37 @@ def extract_scihub(url):
   # different scihub domains since some are not always working
   bases = ['sci-hub.tf', 'scihubtw.tw', 'sci-hub.st', 'sci-hub.scihubtw.tw', 'Sci-Hub.tw', 'Sci-hub.se', 'Sci-hub.do', 'sci-hub.ee']
   
+  user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+  headers = {'user-agent': user_agent}
+  
   #create sci-hub url base for requested paper
   for base in bases:
     try:
       if not url.startswith('http'):
         url = 'https://doi.org/' + url
-      url = 'https://' + base + '/' + url
-      response = requests.get(url)
+      else:
+        url = 'https://' + base + '/' + url
+      
+      response = requests.get(url, headers=headers)
 
-      ### try putting doi.org/ before doi number
-      break
+      
 
-    except:
-      pass
+#     except:
+#       pass
 
   # Get response of sci-hub user requested url page,
   # Then create create soup object of Sci-Hub page.
   # response = requests.get(url)
-  soup = BeautifulSoup(response.content, "lxml")
+      soup = BeautifulSoup(response.content, "lxml")
 
-  # Extract PDF link using BeutifulSoup 
-  try:
-      #Scrape sci-hub page for pdf element and address
+    # Extract PDF link using BeutifulSoup 
+  #   try:
+        #Scrape sci-hub page for pdf element and address
       link = soup.find("iframe", attrs={"id": "pdf"})['src'].split("#")[0]
       if link.startswith('//'): #scraping path for pdf link in sci-hub html frame
           link = link[2:]
           link = 'https://' + link
+      return link
   except Exception:
       return
   
