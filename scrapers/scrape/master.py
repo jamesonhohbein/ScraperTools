c93a93090248882545b0c0c8dd838e2f4614e34c
justinslud <43285909+justinslud@users.noreply.github.com>
2020-11-05 12:54:08 -0500

    add ieee scraper

--- scrapers/scrape/master.py
+++ scrapers/scrape/master.py
@@ -4,6 +4,7 @@ from scrapers.scrape.arxiv import scrape_arxiv
 from scrapers.scrape.core import scrape_core
 from scrapers.scrape.scholar import scrape_scholar
 from scrapers.scrape.springer import scrape_springer
+from scrapers.scrape.ieee import scrape_ieee
 import pandas as pd
 
 db_fctns = {
@@ -12,7 +13,7 @@ db_fctns = {
             'arxiv': scrape_arxiv,
             # 'dpla': scrape_dpla,
             # 'ncbi': scrape_ncbi
-            # 'ieee': scrape_ieee
+            'ieee': scrape_ieee,
             'springer': scrape_springer,
             # 'wiley': scrape_wiley,
             # 'plos': scrape_plos,
